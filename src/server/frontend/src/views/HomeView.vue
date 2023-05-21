ec7acb99883e17288fe382e071d057522e5f3009
Víctor Fernández <victor.fernandez.gabriel@gmail.com>
2023-05-21 21:46:58 +0200

    feat: improve front

--- src/server/frontend/src/views/HomeView.vue
+++ src/server/frontend/src/views/HomeView.vue
@@ -1,26 +1,38 @@
 <script setup lang="ts">
 import SpotifyButton from '@/components/SpotifyButton.vue'
-const lines = [
-  'Spotify has this annoying issue that doesn\'t allow you to share your "Liked Songs" list',
-  'I made this tool to help you with that',
-  "It'll create a new playlist on your account and will keep that playlist in sync with your Liked Songs list",
-  "The list is updated regularly, but it's not instant, so you'll have to wait some time since you change your liked songs list. I'll try to keep that \"regularly\" as low as possible",
-  "The tool will also add a snapshot of the liked songs list each day, for the last 5 days. I'll add a toggle for this in the future"
-]
 </script>
 
 <template>
   <div class="flex flex-col items-center gap-12">
     <div class="flex flex-col gap-8 items-center">
       <h1 class="text-center">Make Spotify Liked playlist shareable</h1>
-      <div id="explanation-wrapper">
-        <p
-          v-for="line of lines"
-          :key="line"
-          class="py-2 px-5 first:pt-4 last:pb-4 border-b last:border-none border-solid border-dark"
-        >
-          {{ line }}
-        </p>
+      <div class="flex gap-5 items-center">
+        <div id="explanation-wrapper">
+          <p
+            class="py-2 px-5 first:pt-4 last:pb-4 border-b last:border-none border-solid border-dark"
+          >
+            Spotify has
+            <a
+              target="_blank"
+              href="https://community.spotify.com/t5/Live-Ideas/Make-quot-Liked-Songs-quot-playlist-shareable/idi-p/4872982"
+              >this annoying issue</a
+            >
+            that doesn't allow you to share your "Liked Songs" list
+          </p>
+          <p
+            class="py-2 px-5 first:pt-4 last:pb-4 border-b last:border-none border-solid border-dark"
+          >
+            This tool will create a new playlist on your account and will keep that playlist in sync
+            with your Liked Songs list (it's not instant)
+          </p>
+          <p
+            class="py-2 px-5 first:pt-4 last:pb-4 border-b last:border-none border-solid border-dark"
+          >
+            Optionally the tool will also add a snapshot of the liked songs list each day, for the
+            last 5 days
+          </p>
+        </div>
+        <img src="../assets/playlists.png" alt="Spotify playlists" class="h-64" />
       </div>
       <SpotifyButton type="link" href="/auth/login">Authorize with Spotify</SpotifyButton>
     </div>
